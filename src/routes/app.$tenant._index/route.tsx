0965c25e86b83a2c0266afef678e544091b33709
Kyle Lambert <klambert002@outlook.com>
2023-06-21 04:14:59 +0930

    Add redirects for root routes

---
+++ src/routes/app.$tenant._index/route.tsx
@@ -0,0 +1,10 @@
+import { authenticator } from "@/lib/services/auth.server";
+import type { LoaderArgs } from "@remix-run/node";
+
+export async function loader({ request, params }: LoaderArgs) {
+  const tenantId = params.tenant as string;
+  return authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+    successRedirect: `/app/${tenantId}/overview`,
+  });
+}
